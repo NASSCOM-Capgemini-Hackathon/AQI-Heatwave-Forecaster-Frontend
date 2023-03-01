7a3f18c9cfbf000644d6c714e3497992fcc0a658
Mathana Mathav <62739618+mathanamathav@users.noreply.github.com>
2023-03-01 20:09:19 +0530

    fixed s3 permission

--- pages/4_📈_Monthly_Predictions.py
+++ pages/4_📈_Monthly_Predictions.py
@@ -7,6 +7,7 @@ import plotly.subplots as sp
 import requests
 import json
 import codecs
+import s3fs
 from pandas import json_normalize
 import numpy as np
 from streamlit_card import card
@@ -14,13 +15,15 @@ from datetime import date
 import utils
 import prophet
 from prophet.serialize import model_from_json
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def print_top3(df):
     col_list = list(st.columns(3, gap="medium"))
     for i in range(len(col_list)):
         with col_list[i]:
-            print(df['Predictions'].iloc[i])
+            # print(df['Predictions'].iloc[i])
             st.metric(pd.to_datetime(df['Date'].iloc[i]).strftime(
                 '%B'), round(df['Predictions'].iloc[i], 2))
 
@@ -38,8 +41,12 @@ def display_aqi(city, slider_col):
     st.markdown("<hr>",
                 unsafe_allow_html=True)
     st.header(city)
-    aqi_city = pd.read_csv(
-        's3://capegemini-hackathon/predictions/aqi/{}/aqi.csv'.format(city), header=0)
+
+    fs = s3fs.S3FileSystem(key='AKIAQOY2QI5NU7SFAHPH',
+                           secret='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')
+    with fs.open('capegemini-hackathon/predictions/aqi/{}/aqi.csv'.format(city)) as f:
+        aqi_city = pd.read_csv(f, header=0)
+
     aqi_city = aqi_city[aqi_city['Date'] < '2024-01-01']
     st.markdown("<hr>",
                 unsafe_allow_html=True)
@@ -65,16 +72,21 @@ def display_aqi(city, slider_col):
 def load_prophet(city):
     with open('models\{}_model.json'.format(city), 'r') as fin:
         m = model_from_json(fin.read())
-    forecasts = pd.read_csv(
-        's3://capegemini-hackathon/prophet/forecasts/pred-{}.csv'.format(city), header=0)
+
+    fs = s3fs.S3FileSystem(key='AKIAQOY2QI5NU7SFAHPH',
+                           secret='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')
+    with fs.open('capegemini-hackathon/prophet/forecasts/pred-{}.csv'.format(city)) as f:
+        forecasts = pd.read_csv(f, header=0)
     forecasts.drop(list(forecasts.columns)[0], axis=1, inplace=True)
     fig1 = prophet.plot.plot_plotly(m, forecasts)
     st.plotly_chart(fig1, use_container_width=True)
 
 
 def display_heatwave(city, slider_col):
-    weather_city = pd.read_csv(
-        's3://capegemini-hackathon/predictions/weather/{}/weather.csv'.format(city), header=0)
+    fs = s3fs.S3FileSystem(key='AKIAQOY2QI5NU7SFAHPH',
+                           secret='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')
+    with fs.open('capegemini-hackathon/predictions/weather/{}/weather.csv'.format(city)) as f:
+        weather_city = pd.read_csv(f, header=0)
     weather_city = weather_city[(
         weather_city['Date'] < '2024-01-01') & (weather_city['Date'] > '2022-12-01')]
     st.markdown("<hr>",
